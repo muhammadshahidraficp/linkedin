64aadbe4e8e4b6623434eac2ff81f85858d4e560
shahidrafi <shahidrafi@tncitgroup.com>
2023-05-22 17:04:35 +0400

    updated

--- src/Components/Organisms/Header/index.tsx
+++ src/Components/Organisms/Header/index.tsx
@@ -13,6 +13,11 @@ interface HeaderOrganismProps { }
 
 const HeaderOrganism: React.FC<HeaderOrganismProps> = () => {
     const [selectedTab, setSelectedTab] = useState("Home");
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+      };
 
     return (
         <div className="sticky top-0  border-b border-gray-200 flex justify-evenly z-10">
@@ -39,10 +44,18 @@ const HeaderOrganism: React.FC<HeaderOrganismProps> = () => {
                 <div onClick={() => setSelectedTab("Notification")} className="flex items-center">
                     <AtomicHeaderOptions Icon={Notifications} title="Notification" isSelected={selectedTab === "Notification" ? true : false} avatar={""} />
                 </div>
-                <div className="flex flex-col items-center cursor-pointer text-gray-500 ml-20 justify-center">
-                    <Avatar src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" className="object-contain-25" />
-                    <h3>Me</h3>
+                <div className="flex flex-col items-center cursor-pointer text-gray-500 ml-20">
+                    <Avatar src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" className="object-contain-25" sizes="10px" />
+                    <h3 onClick={toggleMenu} className="relative">Me</h3>
                 </div>
+                {isMenuOpen && (
+                    <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-lg z-10">
+                    {/* Menu items go here */}
+                    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Item 1</a>
+                    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Item 2</a>
+                    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Item 3</a>
+                    </div>
+                )}
             </div>
         </div>
     )
--- src/Components/Pages/Feed/index.tsx
+++ src/Components/Pages/Feed/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderOrganism from '../../Organisms/Header';
 import FeedPostSection from '../../Organisms/FeedPostSection';
 import ProfileSideBar from '../../Molecules/ProfileSideBar';
@@ -6,19 +6,31 @@ import Widget from "../../Molecules/Widget";
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const Feed: React.FC = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 500);
+    }, []);
+
     return (
-        <div className="">
+        <div className="loader-container">
             <HelmetProvider>
                 <Helmet>
                     <title>Feed | LinkedIn</title>
                 </Helmet>
             </HelmetProvider>
+            {isLoading ? (
+        <div className="loader"></div>
+      ) : (<div>
             <HeaderOrganism />
             <div className='bg-gray-100 flex flex-1/5 mt-10'>
                 <ProfileSideBar key="profile-side-bar" />
                 <FeedPostSection />
                 <Widget />
-            </div>
+            </div> </div>)}
         </div>
     );
 }
