7d995545dbc02d73cddcf190e26ea9ed682105fa
shahidrafi <shahidrafi@tncitgroup.com>
2023-06-01 17:16:21 +0400

    updated

--- src/Components/Pages/Messaging/index.tsx
+++ src/Components/Pages/Messaging/index.tsx
@@ -3,10 +3,16 @@ import FeedPostSection from '../../Organisms/FeedPostSection';
 import ProfileSideBar from '../../Molecules/ProfileSideBar';
 import Widget from "../../Molecules/Widget";
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import { Divider } from '@mui/material';
+import { Search } from '@mui/icons-material';
+import TuneIcon from '@mui/icons-material/Tune';
 
 const Messaging: React.FC = () => {
 
     const [isLoading, setIsLoading] = useState(true);
+    const [selectedTab, setselectedTab] = useState("tab1");
 
     useEffect(() => {
         setTimeout(() => {
@@ -23,13 +29,45 @@ const Messaging: React.FC = () => {
             </HelmetProvider>
             {isLoading ? (
                 <div className="loader"></div>
-            ) : ((<div className='bg-gray-100'>
+            ) : ((<div className='bg-gray-100 h-screen'>
                 <div className='grid grid-cols-4 gap-2 m-auto' style={{ maxWidth: "1500px" }}>
-                    {/* <ProfileSideBar key="profile-side-bar" />
-                    <div className='col-span-2'>
-                        Messaging
+                    <div className='h-50 w-25 bg-white rounded-2xl mt-5'>
+                        <div className='font-bold text-black ml-5 mt-5 flex flex-row justify-between'>
+                            Messaging
+                            <div className='flex justify-between items-center gap-5'>
+                                <MoreHorizIcon/>
+                                <EditNoteIcon/>
+                            </div>
+                        </div>
+                        <Divider style={{marginTop:5}}/>
+                        <div className="my-2 mx-2 flex items-center h-8 rounded-md px-2 bg-eef3f8 focus-within:border-red-500 border justify-between">
+                            <Search />
+                            <input type="text" placeholder='Search mesages' className="bg-eef3f8 border-none outline-none" />
+                            <TuneIcon/>
+                        </div>
+                        <div className="flex border-b">
+                        <div
+                            className={`flex-1 p-4 cursor-pointer font-bold ${
+                                selectedTab === 'tab1' ? 'text-green-500 border-b-2 border-green-600' : ''
+                            }`}
+                            onClick={() => setselectedTab('tab1')}
+                        >
+                           Focused
+                        </div>
+                        <div
+                            className={`flex-1 p-4 cursor-pointer font-bold ${
+                                selectedTab === 'tab2' ? 'text-green-500  border-b-2 border-green-600' : ''
+                            }`}
+                            onClick={() => setselectedTab('tab2')}
+                        >
+                            others
+                        </div>
+                        </div>
                     </div>
-                    <Widget /> */}
+                    <div className='col-span-2 h-50 w-25 bg-white rounded-2xl mt-5'>
+
+                    </div>
+                    <Widget />
                 </div> </div>))}
         </div>
     );
